1af016c39da239041350104140af1d44c1e50cf3
Mauricio Robayo <rfmajo@gmail.com>
2021-09-28 17:43:04 -0500

    docs: improve search
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -2,23 +2,17 @@ import React from "react";
 import styled from "styled-components/macro";
 import twemoji from "twemoji";
 import twemojiIndex from "svg-emojis/twemoji/index.json";
-import openMojiColorIndex from "svg-emojis/openmoji/color/index.json";
-import openMojiBlackIndex from "svg-emojis/openmoji/black/index.json";
+import openMojiIndex from "svg-emojis/openmoji/color/index.json";
 import fxEmojiIndex from "svg-emojis/fxemoji/index.json";
 
-const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
 const EmojiInput = styled.input`
-  font-size: 4rem;
-  height: 5rem;
-  width: 5rem;
+  font-size: 2rem;
+  height: 3rem;
+  width: 3rem;
   border: 4px solid gainsboro;
   border-radius: 0.5rem;
   text-align: center;
-  padding: 1rem;
+  padding: 0.5rem;
   margin: 0.5rem 0;
 `;
 const SmallParagraph = styled.p`
@@ -31,36 +25,39 @@ const Output = styled.div`
 `;
 
 function App() {
+  const prefix = "svg-emojis";
   const [emoji, setEmoji] = React.useState("");
-  const [twemojiFiles, setTwemojiFiles] = React.useState<string[]>([]);
-  const [openMojiColorFiles, setOpenMojiColorFiles] = React.useState<string[]>(
-    []
-  );
-  const [openMojiBlackFiles, setOpenMojiBlackFiles] = React.useState<string[]>(
-    []
-  );
-  const [fxEmojiFiles, setFxEmojiFiles] = React.useState<string[]>([]);
+  const [emojis, setEmojis] = React.useState<
+    { name: string; file: string; src: string }[]
+  >([]);
 
   const emojiHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
     if (/\p{Extended_Pictographic}/u.test(value)) {
       const codePoint = twemoji.convert.toCodePoint(value);
-      setTwemojiFiles(
-        twemojiIndex.filter((emoji) => emoji.includes(codePoint))
-      );
-      setFxEmojiFiles(
-        fxEmojiIndex.filter((emoji) => emoji.toLowerCase().includes(codePoint))
-      );
-      setOpenMojiColorFiles(
-        openMojiColorIndex.filter((emoji) =>
-          emoji.toLowerCase().includes(codePoint)
-        )
-      );
-      setOpenMojiBlackFiles(
-        openMojiBlackIndex.filter((emoji) =>
-          emoji.toLowerCase().includes(codePoint)
-        )
-      );
+      setEmojis([
+        {
+          name: "Twemoji",
+          file: twemojiIndex.find((emoji) => emoji.includes(codePoint)) || "",
+          src: `https://twemoji.maxcdn.com/svg/`,
+        },
+        {
+          name: "FxEmoji",
+          file:
+            fxEmojiIndex.find((emoji) =>
+              emoji.toLowerCase().includes(codePoint)
+            ) || "",
+          src: `https://raw.githubusercontent.com/mozilla/fxemoji/gh-pages/svgs/FirefoxEmoji/`,
+        },
+        {
+          name: "OpenMoji",
+          file:
+            openMojiIndex.find((emoji) =>
+              emoji.toLowerCase().includes(codePoint)
+            ) || "",
+          src: `https://www.openmoji.org/data/color/svg/`,
+        },
+      ]);
     }
     setEmoji(value);
   };
@@ -70,7 +67,7 @@ function App() {
       <header>
         <h1>SVG Emojis</h1>
       </header>
-      <Main>
+      <main>
         <label htmlFor="emoji">Emoji</label>
         <EmojiInput
           id="emoji"
@@ -80,44 +77,22 @@ function App() {
           value={emoji}
         />
         <SmallParagraph>
-          Copy and paste from:{" "}
+          Copy and paste from{" "}
           <a href="https://getemoji.com">https://getemoji.com</a>
         </SmallParagraph>
         <Output>
-          <section>
-            <h2>Twemoji</h2>
-            <ul>
-              {twemojiFiles.map((file) => (
-                <li key={file}>{file}</li>
-              ))}
-            </ul>
-          </section>
-          <section>
-            <h2>FxEmoji</h2>
-            <ul>
-              {fxEmojiFiles.map((file) => (
-                <li key={file}>{file}</li>
-              ))}
-            </ul>
-          </section>
-          <section>
-            <h2>OpenMoji Color</h2>
-            <ul>
-              {openMojiColorFiles.map((file) => (
-                <li key={file}>{file}</li>
-              ))}
-            </ul>
-          </section>
-          <section>
-            <h2>OpenMoji Black</h2>
-            <ul>
-              {openMojiBlackFiles.map((file) => (
-                <li key={file}>{file}</li>
-              ))}
-            </ul>
-          </section>
+          {emojis.map(({ name, file, src }) => {
+            return (
+              <section key={file}>
+                <h2>
+                  {name} <img src={`${src}/${file}`} alt={file} />
+                </h2>
+                <code>{`${prefix}/${name}/${file}`}</code>
+              </section>
+            );
+          })}
         </Output>
-      </Main>
+      </main>
     </div>
   );
 }
