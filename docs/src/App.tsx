6fc9e3152d82333d972cd8a7449caa27e9b7ef06
Mauricio Robayo <rfmajo@gmail.com>
2021-09-27 21:24:25 -0500

    add docs
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ docs/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>React Emoji SVG Components</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
